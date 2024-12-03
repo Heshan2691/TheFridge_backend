90312b4bd50a2a679fed43bd7d3d527db9a275e5
Heshan Yatigammana <131267985+Heshan2691@users.noreply.github.com>
2024-12-03 12:34:03 +0530

    make some changes on endpoints

--- TheFridge/TheFridge/obj/Debug/net8.0/TheFridge.AssemblyInfo.cs
+++ TheFridge/TheFridge/obj/Debug/net8.0/TheFridge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheFridge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddb4b9282460e7dc464ab2b961476761df09e3b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db9ce20a5012470f16cee2b556c3f80037cb63ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheFridge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheFridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
